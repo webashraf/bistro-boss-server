cca106073a5b594c4c6d62f1f425948299eb42fe
webashraf <aliashraftamim@gmail.com>
2023-05-26 19:49:18 +0600

    add mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -7,6 +8,63 @@ const port = process.env.PORT || 5000;
 // Middle were //
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.37yfgb3.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res)=> {
     res.send("Bistro boss is running");
