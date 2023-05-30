b08ebc1754d66afaa770c4712b61c266e7a460ac
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2023-05-30 21:44:23 +0600

    add delete oparetion

--- index.js
+++ index.js
@@ -56,7 +56,13 @@ async function run() {
       const result = await cartsCollection.insertOne(data);
       res.send(result);
     })
-
+    app.delete('cart/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = {_id: new ObjectId(id)};
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
